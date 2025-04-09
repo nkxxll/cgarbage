1830aa0ef5473e994f4a19beb9921cb188c216f2
nkxxll <niki.nice1203@gmail.com>
2025-04-09 23:06:16 +0200

    task two

--- refcounting/main.c
+++ refcounting/main.c
@@ -4,6 +4,34 @@
 #include "munit.h"
 #include "snekobject.h"
 
+MunitResult test_inc_refcount(const MunitParameter* pointer, void* key)
+{
+    snek_object_t* obj = new_snek_integer(10);
+    munit_assert_int(obj->refcount, ==, 1);
+
+    refcount_inc(obj);
+    munit_assert_int(obj->refcount, ==, 2);
+
+    free(obj);
+    return MUNIT_OK;
+}
+
+MunitResult test_inc_refcount_more(const MunitParameter* pointer, void* key)
+{
+    snek_object_t* obj = new_snek_float(4.20);
+    munit_assert_int(obj->refcount, ==, 1);
+
+    refcount_inc(obj);
+    refcount_inc(obj);
+    refcount_inc(obj);
+    refcount_inc(obj);
+    refcount_inc(obj);
+    munit_assert_int(obj->refcount, ==, 6);
+
+    free(obj);
+    return MUNIT_OK;
+}
+
 MunitResult test_int_has_refcount(const MunitParameter* pointer, void* key)
 {
     snek_object_t* obj = new_snek_integer(10);
@@ -20,11 +48,21 @@ MunitResult test_float_has_refcount(const MunitParameter* pointer, void* key)
     return MUNIT_OK;
 }
 
+MunitResult test_null_obj(const MunitParameter* pointer, void* key)
+{
+    refcount_inc(NULL);
+    munit_assert(1);
+    return MUNIT_OK;
+}
+
 int main(void)
 {
     MunitTest tests[] = {
         munit_test("test_int_has_refcount", test_int_has_refcount),
         munit_test("test_float_has_refcount", test_float_has_refcount),
+        munit_test("test_null_obj", test_null_obj),
+        munit_test("test_int_has_refcount", test_int_has_refcount),
+        munit_test("test_inc_refcount", test_inc_refcount),
         munit_null_test,
     };
 
--- refcounting/snekobject.c
+++ refcounting/snekobject.c
@@ -3,11 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+void refcount_inc(snek_object_t* obj)
+{
+    if (obj == NULL)
+    {
+        return;
+    }
+    obj->refcount++;
+}
+
 snek_object_t* _new_snek_object()
 {
-    snek_object_t* obj = (snek_object_t*)malloc(sizeof(snek_object_t));
-    memset(obj, 0, sizeof(snek_object_t));
+    snek_object_t* obj = calloc(1, sizeof(snek_object_t));
+    if (obj == NULL)
+    {
+        return NULL;
+    }
+
     obj->refcount = 1;
+
     return obj;
 }
 
