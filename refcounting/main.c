0060705a6a29445d3b229d809a0243b85f6e1d5b
nkxxll <niki.nice1203@gmail.com>
2025-04-09 22:39:53 +0200

    makefile and refcounting first task done

--- refcounting/main.c
+++ refcounting/main.c
@@ -4,26 +4,32 @@
 #include "munit.h"
 #include "snekobject.h"
 
-munit_case(RUN, test_int_has_refcount, {
-  snek_object_t *obj = new_snek_integer(10);
-  munit_assert_int(obj->refcount, ==, 1);
-  free(obj);
-});
+MunitResult test_int_has_refcount(const MunitParameter* pointer, void* key)
+{
+    snek_object_t* obj = new_snek_integer(10);
+    munit_assert_int(obj->refcount, ==, 1);
+    free(obj);
+    return MUNIT_OK;
+}
 
-munit_case(SUBMIT, test_float_has_refcount, {
-  snek_object_t *obj = new_snek_float(42.0);
-  munit_assert_int(obj->refcount, ==, 1);
-  free(obj);
-});
+MunitResult test_float_has_refcount(const MunitParameter* pointer, void* key)
+{
+    snek_object_t* obj = new_snek_float(42.0);
+    munit_assert_int(obj->refcount, ==, 1);
+    free(obj);
+    return MUNIT_OK;
+}
 
-int main() {
-  MunitTest tests[] = {
-      munit_test("test_int_has_refcount", test_int_has_refcount),
-      munit_test("test_float_has_refcount", test_float_has_refcount),
-      munit_null_test,
-  };
+int main(void)
+{
+    MunitTest tests[] = {
+        munit_test("test_int_has_refcount", test_int_has_refcount),
+        munit_test("test_float_has_refcount", test_float_has_refcount),
+        munit_null_test,
+    };
 
-  MunitSuite suite = munit_suite("refcount", tests);
+    MunitSuite suite = munit_suite("refcount", tests);
 
-  return munit_suite_main(&suite, NULL, 0, NULL);
+    munit_suite_main(&suite, NULL, 0, NULL);
+    return EXIT_SUCCESS;
 }
--- refcounting/snekobject.c
+++ refcounting/snekobject.c
@@ -1,87 +1,105 @@
+#include "snekobject.h"
+
 #include <stdlib.h>
 #include <string.h>
 
-#include "snekobject.h"
-
-snek_object_t *_new_snek_object() {
-  // ?
+snek_object_t* _new_snek_object()
+{
+    snek_object_t* obj = (snek_object_t*)malloc(sizeof(snek_object_t));
+    memset(obj, 0, sizeof(snek_object_t));
+    obj->refcount = 1;
+    return obj;
 }
 
 // don't touch below this line
 
-snek_object_t *new_snek_array(size_t size) {
-  snek_object_t *obj = _new_snek_object();
-  if (obj == NULL) {
-    return NULL;
-  }
+snek_object_t* new_snek_array(size_t size)
+{
+    snek_object_t* obj = _new_snek_object();
+    if (obj == NULL)
+    {
+        return NULL;
+    }
 
-  snek_object_t **elements = calloc(size, sizeof(snek_object_t *));
-  if (elements == NULL) {
-    free(obj);
-    return NULL;
-  }
+    snek_object_t** elements = calloc(size, sizeof(snek_object_t*));
+    if (elements == NULL)
+    {
+        free(obj);
+        return NULL;
+    }
 
-  obj->kind = ARRAY;
-  obj->data.v_array = (snek_array_t){.size = size, .elements = elements};
+    obj->kind         = ARRAY;
+    obj->data.v_array = (snek_array_t){.size = size, .elements = elements};
 
-  return obj;
+    return obj;
 }
 
-snek_object_t *new_snek_vector3(snek_object_t *x, snek_object_t *y,
-                                snek_object_t *z) {
-  if (x == NULL || y == NULL || z == NULL) {
-    return NULL;
-  }
+snek_object_t* new_snek_vector3(snek_object_t* x,
+                                snek_object_t* y,
+                                snek_object_t* z)
+{
+    if (x == NULL || y == NULL || z == NULL)
+    {
+        return NULL;
+    }
 
-  snek_object_t *obj = _new_snek_object();
-  if (obj == NULL) {
-    return NULL;
-  }
+    snek_object_t* obj = _new_snek_object();
+    if (obj == NULL)
+    {
+        return NULL;
+    }
 
-  obj->kind = VECTOR3;
-  obj->data.v_vector3 = (snek_vector_t){.x = x, .y = y, .z = z};
+    obj->kind           = VECTOR3;
+    obj->data.v_vector3 = (snek_vector_t){.x = x, .y = y, .z = z};
 
-  return obj;
+    return obj;
 }
 
-snek_object_t *new_snek_integer(int value) {
-  snek_object_t *obj = _new_snek_object();
-  if (obj == NULL) {
-    return NULL;
-  }
+snek_object_t* new_snek_integer(int value)
+{
+    snek_object_t* obj = _new_snek_object();
+    if (obj == NULL)
+    {
+        return NULL;
+    }
 
-  obj->kind = INTEGER;
-  obj->data.v_int = value;
-  return obj;
+    obj->kind       = INTEGER;
+    obj->data.v_int = value;
+    return obj;
 }
 
-snek_object_t *new_snek_float(float value) {
-  snek_object_t *obj = _new_snek_object();
-  if (obj == NULL) {
-    return NULL;
-  }
+snek_object_t* new_snek_float(float value)
+{
+    snek_object_t* obj = _new_snek_object();
+    if (obj == NULL)
+    {
+        return NULL;
+    }
 
-  obj->kind = FLOAT;
-  obj->data.v_float = value;
-  return obj;
+    obj->kind         = FLOAT;
+    obj->data.v_float = value;
+    return obj;
 }
 
-snek_object_t *new_snek_string(char *value) {
-  snek_object_t *obj = _new_snek_object();
-  if (obj == NULL) {
-    return NULL;
-  }
+snek_object_t* new_snek_string(char* value)
+{
+    snek_object_t* obj = _new_snek_object();
+    if (obj == NULL)
+    {
+        return NULL;
+    }
 
-  int len = strlen(value);
-  char *dst = malloc(len + 1);
-  if (dst == NULL) {
-    free(obj);
-    return NULL;
-  }
+    int   len = strlen(value);
+    char* dst = malloc(len + 1);
+    if (dst == NULL)
+    {
+        free(obj);
+        return NULL;
+    }
 
-  strcpy(dst, value);
+    strcpy(dst, value);
 
-  obj->kind = STRING;
-  obj->data.v_string = dst;
-  return obj;
+    obj->kind          = STRING;
+    obj->data.v_string = dst;
+    return obj;
 }
